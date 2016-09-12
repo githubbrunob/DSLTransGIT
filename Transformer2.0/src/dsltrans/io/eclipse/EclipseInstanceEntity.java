89ee8d481ac06266820f4b4fef94f520366fdf8e
Cláudio Gomes <cla.gms@gmail.com>
2016-09-12 14:41:05 +0200

    constructor

--- Transformer2.0/src/dsltrans/io/eclipse/EclipseInstanceEntity.java
+++ Transformer2.0/src/dsltrans/io/eclipse/EclipseInstanceEntity.java
@@ -1,8 +1,16 @@
 package dsltrans.io.eclipse;
 
+import org.eclipse.emf.ecore.impl.EModelElementImpl;
+
+import dsltrans.metamodel.MetaEntity;
 import dsltrans.model.InstanceEntity;
 
 
 public class EclipseInstanceEntity extends InstanceEntity {
+
+	public EclipseInstanceEntity(EModelElementImpl object, MetaEntity entity) {
+		super(object, entity);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
